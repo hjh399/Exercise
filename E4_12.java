76752740ab680a00f2f60857b8cc30c484783a67
LILY <lily@LILY-no-MacBook-Pro.local>
2015-03-10 21:30:59 +0800

    E4_12 难度1-考求最大值
    1.n从100开始试

---
+++ E4_12.java
@@ -0,0 +1,12 @@
+public class E4_12 {
+  public static void main(String[] args) {
+	  int n = 100;
+	  
+	  while (n * n <= 12000)
+	  {
+		  n ++;
+	  }
+	  
+	  System.out.println("The number is " + n);      	  	  
+  }
+}	  
