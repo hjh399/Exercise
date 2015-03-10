82a5713fbca902595913f01fa181e2435e1fe147
LILY <lily@LILY-no-MacBook-Pro.local>
2015-03-10 16:16:28 +0800

    E4_4 难度低-考与E4_3相同-
    1.学习参考答案的while做法和\t\t用法

---
+++ E4_4.java
@@ -0,0 +1,17 @@
+public class E4_4 {
+  public static void main(String[] args) {
+	  System.out.print("‰p—¢       ç•Ä");
+	  for (int mile = 1; mile < 11 ; mile ++ )
+	  {
+		  double km = mile * 1.609;
+		  System.out.printf("\n%-10d%1.3f",mile,km);
+	  }
+	  
+//	    int miles = 1; 
+//	    while (miles <= 10) {
+//	      System.out.println(miles + "\t\t" + miles * 1.609);
+//	      miles++;
+//	    }
+	  
+  }
+}
