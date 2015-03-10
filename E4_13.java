b3e922fa199811db34e946848e0f5df7a0e0affb
LILY <lily@LILY-no-MacBook-Pro.local>
2015-03-10 21:40:57 +0800

    E4_13 难度1-考上一题同类型

---
+++ E4_13.java
@@ -0,0 +1,12 @@
+public class E4_13 {
+  public static void main(String[] args) {
+	  int n = 30;
+	  
+	  while (n * n * n >= 12000)
+	  {
+		  n --;
+	  }
+	  
+	  System.out.println("The number is " + n);      	  	  
+  }
+}	  
