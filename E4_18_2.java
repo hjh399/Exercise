303c1e241ed836af36f15f606b809a08c2f0fc31
LILY <lily@LILY-no-MacBook-Pro.local>
2015-03-11 22:07:20 +0800

    E4_18_2 难度2-考嵌套循环
    1.这次改的是第一层行的循环

---
+++ E4_18_2.java
@@ -0,0 +1,14 @@
+public class E4_18_2 {
+  public static void main(String[] args) {
+	  for ( int i = 6; i > 0 ; i--)
+	  {  
+		  for (int k = 1 ; k <= i  ; k ++)
+		  { 
+		      System.out.print(k + "\t");          
+		  }
+
+		  System.out.println(" ");
+	  }
+
+  }
+}	  
