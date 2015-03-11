264519f0c43ba40931947f2937b0adffd939e6da
LILY <lily@LILY-no-MacBook-Pro.local>
2015-03-11 22:01:01 +0800

    E4_18_1 难度1-考简单嵌套循环
    1.跟前一题相比这一题弱爆了

---
+++ E4_18_1.java
@@ -0,0 +1,14 @@
+public class E4_18_1 {
+  public static void main(String[] args) {
+	  for ( int i = 1; i <= 6 ; i++)
+	  {  
+		  for (int k = 1 ; k <= i  ; k ++)
+		  { 
+		      System.out.print(k + "\t");          
+		  }
+
+		  System.out.println(" ");
+	  }
+
+  }
+}	  
