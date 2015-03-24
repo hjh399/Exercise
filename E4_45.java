7b5a74c19932a0c2666529237487aae4efffe950
LILY <lily@LILY-no-MacBook-Pro.local>
2015-03-24 19:39:01 +0800

    E4_45 难度1-考嵌套循环
    1.有一种切菜的感觉

---
+++ E4_45.java
@@ -0,0 +1,19 @@
+public class E4_45 {
+  public static void main(String[] args) {
+	  
+	  int sum = 0;
+	  for (int i = 1 ; i < 8 ; i ++)
+	  {
+		  for (int j = 1 ; j < 8 ; j ++)
+		  {
+			  System.out.println(i + " " + j);
+			  sum ++ ;
+		  }
+	  }
+	   
+	  System.out.println("The sum is " + sum);
+  }
+}
+
+
+
