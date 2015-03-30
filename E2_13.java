a72e0d5b911a5e3886164b115d9fb1d8793ef40a
LILY <lily@LILY-no-MacBook-Pro.local>
2015-03-30 22:50:51 +0800

    E2_13 第三次修改
    1.把年月换一遍，详见5_7

--- E2_13.java
+++ E2_13.java
@@ -9,15 +9,15 @@ public class E2_13 {
 		double investmentAmount = input.nextDouble();
 		
 		
-		System.out.print("Enter monthly interest rate:");
-		double monthlyInterestRate = input.nextDouble();
-		double annuallyInterestRate= monthlyInterestRate / 12;
+		System.out.print("Enter annual interest rate:");
+		double annualInterestRate = input.nextDouble();
+		double monthlyInterestRate= annualInterestRate / 12;
 		
 
 		System.out.print("Enter number of years:");
 		int years = input.nextInt();
 		
-		double futureInvestmentValue = investmentAmount * Math.pow((1 + annuallyInterestRate / 100.0 ) , years * 12);
+		double futureInvestmentValue = investmentAmount * Math.pow((1 + monthlyInterestRate / 100.0 ) , years * 12);
 		System.out.printf("%2.2f",futureInvestmentValue);
 	}
 }
