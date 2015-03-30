88f5d3ccaac3ff83bd26281f2182f583f38dad02
LILY <lily@LILY-no-MacBook-Pro.local>
2015-03-30 22:16:28 +0800

    E5_7 难度6.5-考修改程序
    1.因为2_13的程序问题，搞了好久
    1.1实际上是输入年利率，转换成月利率算出月复利的总年未来投资额
    1.2 2_13的程序里年月弄混了
    2.github把程序的不同点显示出来，感觉真好

--- E5_7.java
+++ E5_7.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class E5_7 {
   public static void main(String[] args) {
-	  int year = 1;
+	  int year = 30;
 	  int i;
 	  
 	  Scanner input = new Scanner(System.in);
 	  System.out.print("The amount invested: ");
 	  double investmentAmount = input.nextDouble();
 	  System.out.print("Annual interest rate: ");
-	  double annuallyInterestRate = input.nextDouble();	  
-	  double monthlyInterestRate = annuallyInterestRate / 12;
+	  double annuallyInterestRate = input.nextDouble();	
+	  double monthlyInterestRate = annuallyInterestRate / 12.0;	 
 	  
 	  System.out.println("Year\t\t\tFuture Value");
 	  for (i = 1 ; i <= year ; i ++ )
 	  {	  
-	  System.out.println(i + "\t\t\t" + futureInvestmentValue(investmentAmount, annuallyInterestRate, i));
+	  System.out.printf(i + "\t\t\t%8.2f\n",futureInvestmentValue(investmentAmount, monthlyInterestRate, i));
 	  }
 	  
 	}
