3bd0dd69a5a5668f052c4903ad0104dd477de53c
hjh399 <lilyhuang399@gmail.com>
2015-04-02 22:56:17 +0800

    E5_18 难度1.5-考Math.sqrt方法

---
+++ E5_18.java
@@ -0,0 +1,11 @@
+public class E5_18 {
+  public static void main(String[] args) {
+	  
+	  System.out.println("数字\t\t平方根");
+	  
+	  for (int i = 0 ; i <= 20 ; i += 2)
+	  {
+		  System.out.printf(i + "\t\t%2.4f\n",Math.sqrt(i));
+	  }
+	}   
+} 
