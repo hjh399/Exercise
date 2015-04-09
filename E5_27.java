67f95833e00b19f430a6f9c584f9e100e692cbe6
hjh399 <lilyhuang399@gmail.com>
2015-04-09 17:53:52 +0800

    E5_27 难度2-考反素数
    1.与上一题异曲同工，改一下就好了

---
+++ E5_27.java
@@ -0,0 +1,49 @@
+public class E5_27 {
+	public static void main(String[] args) {		
+		for (int i = 1, number = 2; i <= 100;number ++)
+		{
+			if (isPrimeNumber(reverse(number)) && isPrimeNumber(number) && isNotPalindrome(number) )
+			{				
+				System.out.printf("%6d", number);
+				
+				String s = (i % 10 == 0) ? "\n" : "\t";
+				System.out.print(s);
+				i ++;
+			}
+		}		
+	}
+	
+	//	Return the reversal of an integer, i.e. reverse(456) returns 654  
+	public static int reverse(int number) {	  
+		String sum = "";	  
+		while ( number != 0 )
+		{
+			int digit = number % 10;
+			sum = sum + digit;
+			number /= 10;
+		}		
+		int reverse = Integer.parseInt(sum);
+		return reverse;
+	  }
+	  
+	//  Return true if number is not palindrome
+	public static boolean isNotPalindrome(int number) {	  	
+		return (reverse(number) == number)? false : true;		  
+	  }
+	
+	//	Return true if number is primeNumber
+	public static boolean isPrimeNumber(int number) {	
+		boolean isPrimeNumber = true;
+		
+		for (int divisor = 2; divisor <= number / 2; divisor++) 
+		{
+		    if (number % divisor == 0) 
+		    { // If true, number is not prime
+		          isPrimeNumber = false; // Set isPrime to false          
+		       break; // Exit the for loop
+		    }		    
+		}	 
+		
+		return isPrimeNumber;	    
+	}
+} 
